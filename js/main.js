4898c9fa6faee6a2ffc360b77010ca068e61d96a
naghmeh <melodycharitable4774@gmail.com>
2024-09-07 11:34:52 +0200

    finished the API and called it in js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+fetch('products.json')
+
+.then(response => response.json())
+
+.then(data => {
+    console.log(data)
+
+    data.forEach(item => {
+        document.getElementById('products').innerHTML += `
+        
+        <div class="product">
+
+        <img src="../${item.image}">
+        <h3>${item.name}</h3>
+        <p>Cat: ${item.category}</p>
+        <p>Price: $${item.price}</p>
+
+        </div>
+        `
+    });
+})
